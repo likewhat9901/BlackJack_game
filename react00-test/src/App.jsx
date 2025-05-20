0702a3cfab5cf249df6ab00318726a9d68d34217
likewhat <parkko123@naver.com>
2025-05-21 00:22:23 +0900

    111

--- react00-test/src/App.jsx
+++ react00-test/src/App.jsx
@@ -73,43 +73,41 @@ function App() {
 
   return (
     <section>
-      <h1>블랙잭 게임</h1>
-      <div>
-        <div className="play computer">
-          <div className="card computer" id="c_first">카드1</div>
-          <div className="card computer" id="c_second">카드2</div>
-          <div className="card computer" id="c_third">카드3</div>
-          <div className="card computer" id="c_fourth">카드4</div>
-          <div className="card computer" id="c_fifth">카드5</div>
-        </div>
-        <div className="deck">
-          <div className="card">카드덱</div>
-          <p className="rule">
-            <strong>게임 규칙</strong><br />
-            - 목표: 21점을 넘지 않고 딜러보다 높은 점수 얻기<br />
-            - A: 1점 또는 11점<br />
-            - J/Q/K: 10점<br />
-          </p>
-        </div>
-        <div className="play user">
-          <div className="card player">카드1</div>
-          <div className="card player">카드2</div>
-          <div className="card player">카드3</div>
-          <div className="card player">카드4</div>
-          <div className="card player">카드5</div>
-        </div>
-        <div className="control">
-          <button type="button" className="button gameStart" onClick={game_start}>게임시작</button>
-          <button type="button" className="button double">더블다운</button>
-          <button type="button" className="button hit" onClick={hit}>히트(한장더)</button>
-          <button type="button" className="button stand" onClick={stand}>스탠드(Stop)</button>
-          <div className="money">
-            <div className="show bet">
-              <label htmlFor="bet_input">베팅금액: </label>
-              <input type="text" id="bet_input" />
-            </div>
-            <div className="show myBudget">남은금액 : <span id="deposit">60,000</span></div>
+      <h1 className='header'>블랙잭 게임</h1>
+      <div className="play computer">
+        <div className="card computer" id="c_first">카드1</div>
+        <div className="card computer" id="c_second">카드2</div>
+        <div className="card computer" id="c_third">카드3</div>
+        <div className="card computer" id="c_fourth">카드4</div>
+        <div className="card computer" id="c_fifth">카드5</div>
+      </div>
+      <div className="deck">
+        <div className="card">카드덱</div>
+        <p className="rule">
+          <strong>게임 규칙</strong><br />
+          - 목표: 21점을 넘지 않고 딜러보다 높은 점수 얻기<br />
+          - A: 1점 또는 11점<br />
+          - J/Q/K: 10점<br />
+        </p>
+      </div>
+      <div className="play user">
+        <div className="card player">카드1</div>
+        <div className="card player">카드2</div>
+        <div className="card player">카드3</div>
+        <div className="card player">카드4</div>
+        <div className="card player">카드5</div>
+      </div>
+      <div className="control">
+        <button type="button" className="button gameStart" onClick={game_start}>게임시작</button>
+        <button type="button" className="button double">더블다운</button>
+        <button type="button" className="button hit" onClick={hit}>히트(한장더)</button>
+        <button type="button" className="button stand" onClick={stand}>스탠드(Stop)</button>
+        <div className="money">
+          <div className="show bet">
+            <label htmlFor="bet_input">베팅금액: </label>
+            <input type="text" id="bet_input" />
           </div>
+          <div className="show myBudget">남은금액 : <span id="deposit">60,000</span></div>
         </div>
       </div>
     </section>
